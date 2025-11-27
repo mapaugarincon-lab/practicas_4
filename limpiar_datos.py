30c661b78e7c6dd0d4f1ad1837d8e29e732a7348
Maria <mapaugarincon@gmail.com>
2025-11-27 10:25:54 -0500

    Delete limpiar_datos.py

--- limpiar_datos.py
+++
@@ -1,6 +0,0 @@
-class Limpiar_los_datos: 
-    def limpiar_none(fila):
-        for clave, valor in fila.items():
-            if valor == "NA":
-                fila[clave] = None
-            return fila
